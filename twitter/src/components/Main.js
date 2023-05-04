e7f42fe1c7a05c3545c556f7cf1a104d80bd97fe
santiago <contatofasantiago@hotmail.com>
2023-05-04 15:22:27 -0300

    added styling and new button functionality

--- twitter/src/components/Main.js
+++ twitter/src/components/Main.js
@@ -2,6 +2,7 @@
 
 import React, { useState } from "react";
 import '../design/main_style.css'
+import CheckedProfile from "./CheckedProfile";
 
 export default function Main() {
   const [inputContent, setInputContent] = useState('')
@@ -19,16 +20,15 @@ export default function Main() {
     setStorageContent(newStorageContent)
   }
 
-  const remainingChars = 250 - inputContent.length
+  const remainingChars = 140 - inputContent.length
 
   const profileInfo = {
     nameUser: 'Santiago | DEV',
     nickName: '@devsantiag',
-    userImage: 'https://pbs.twimg.com/profile_images/1644894643660759040/J4whV6qJ_400x400.jpg',
-    checked: null
+    userImage: 'https://pbs.twimg.com/profile_images/1644894643660759040/J4whV6qJ_400x400.jpg'
   }
   function hundleSendContent(event) {
-    if (inputContent.length < 251 && inputContent.length > 10) {
+    if (inputContent.length <= 140) {
       if (event.key === 'Enter') {
         event.preventDefault()
         setStorageContent([
@@ -37,10 +37,12 @@ export default function Main() {
         setInputContent('')
       }
       setWarning('')
-    } else if (!inputContent.length < 251) {
-      setWarning(<p className="colorWarningLimiteText">The number of characters is greater than 250. Please try again!</p>)
+    } else if (inputContent.length >= 0) { //corrigir 
+      inputContent.substring(0, 140)
+      setWarning(<p className="colorWarningLimiteText">The number of characters is greater than 140. Please try again!</p>)
     }
   }
+
   return (
     <div className="styleDivContainer">
       <form onSubmit={hundleSendContent}>
@@ -65,9 +67,11 @@ export default function Main() {
           <ul key={indexId}>
             <li className="liContentStyle">
               <img src={profileInfo.userImage} className="imageUser" alt="userImage" />
-              <p className="nameUser"> {profileInfo.nameUser} {profileInfo.nickName} </p>
+              <p className="nameUser"> {profileInfo.nameUser} </p>
+              <p className="userNickName">{profileInfo.nickName}</p>
               <p className="textContentPosted" >
                 {postStorage}
+                <CheckedProfile/>
                 <button
                   className="individualDeleteContent"
                   type="button"
